3ce4b0d0383e094dffb806a41cc74684644d9cab
MatveyPRO32 <matveyrotte@gmail.com>
2023-11-03 11:54:06 +0300

    3

--- test.py
+++ test.py
@@ -1,1 +1,5 @@
 import spectrometer_api as spec_api 
+import visualizer as vis
+
+print(spec_api.measure_and_get_data())
+vis.build_frequency_intensities(spec_api.measure_and_get_data())
--- visualizer.py
+++ visualizer.py
@@ -1,8 +1,7 @@
 import plotly.express as px
 
-
-fig = px.line(x=data1.keys(),y=data1.values())
-fig2 = px.line(x=data2.keys(),y=data2.values())
-
-fig.show()
-fig2.show()
+def build_frequency_intensities(data:dict):
+    fig = px.line(x=data.keys(),y=data.values())
+    fig.show()
+    
+    
