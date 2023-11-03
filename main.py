a3ecdf1d4541615592e0727ecea048adad741898
MatveyPRO32 <matveyrotte@gmail.com>
2023-11-03 18:05:02 +0300

    saving data to files added

--- main.py
+++ main.py
@@ -3,14 +3,20 @@ from matplotlib.animation import FuncAnimation
 import numpy as np
 
 import spectrometer_api as spec_api
-import visualizer as vis7
 
+from datetime import datetime as dt
+from os import makedirs
+
+save_spectrums = True
+
+makedirs("output",exist_ok=True)
 
 def absorption_spectrum_processing(x, point_1,point_2,point_3):
-    return np.log10((x-point_1-point_3) / (point_2-point_1-point_3)) * (-1)  
-
+    return np.log10((x-point_1-point_3) / (point_2-point_1-point_3)) * (-20)  
 absorption_spectrum_processing = np.vectorize(absorption_spectrum_processing)
 
+def save_spectrums_to_file(intensities):
+    np.savetxt(f"output\\{dt.now()}_spectrum.csv".replace(":","_"), np.column_stack((wave_lengths, intensities)), delimiter=",")
 
 input("Waiting to measure darkness, press enter to capture.")
 point_1 = spec_api.measure_and_get_data(return_2D_array_with_wave_length=True)
@@ -35,9 +41,11 @@ ax.set_ylim(2200, 4000)
 def update(frame):
     data = spec_api.measure_and_get_data()
     data = absorption_spectrum_processing(data,point_1,point_2,point_3)
+    if save_spectrums:
+        save_spectrums_to_file(data)
     ax.clear()  # Clear the current axes
     ax.plot(wave_lengths, data)  # Redraw the plot with updated data
-    ax.set_ylim(-2.5,2.5)
+    ax.set_ylim(-45,45)
     ax.set_xlim(400,700)
 
 ani = FuncAnimation(fig, update, interval=1)
